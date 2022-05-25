7ef62192d5bd55177a131265f5153a88c66b569a
resalen <resalen2@hotmail.com>
2022-05-24 21:55:39 -0500

    con Const no podemos cambiar la variable

--- fundModer.js
+++ fundModer.js
@@ -1,13 +1,21 @@
 //var vs let vs const
 //La Constante es una variable que no cambia
-//La constante también vive dentro del scope
+//La diferencia entre const y let
 
+//si ponemos:
+/*
 const edad = 10
-if(true){
-    const edad = 20
-    console.log(edad)
-}
-console.log(edad)
-//dentro de la condcional nos pinta 20. Esta vriable está viviendo dentro del if, y una vez que se sale de este scope if, muere.
-//fuera de la condicional nos pinta 10. Esto está de forma global
-//Ambas const aunque tienen el mismo nombre son 2 variables diferentes por vivir en diferentes scopes
+const edad = 20
+//Nos marca error en consola 
+// lo mismo que pasa con let*/
+
+//Con let podíamos sobre escribir esa variable:
+/*
+let edad = 10
+edad = 20 */
+
+//pero con una constante no podemos sobre escribir esa variable:
+/*
+const edad = 10
+edad = 20 */
+
