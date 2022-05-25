51e19279318ec305100e5e74850893579bdf7cab
resalen <resalen2@hotmail.com>
2022-05-24 21:08:33 -0500

    let pinta sus valores de scope

--- fundModer.js
+++ fundModer.js
@@ -1,12 +1,12 @@
 //var vs let vs const
 //Trabajando con let y bar
 
-var edad = 10
+let edad = 10
 if(true){
-    var edad = 20
+    let edad = 20
     console.log(edad)
 }
 console.log(edad)
-//dentro de la condcional nos pinta 20
-//fuera de la condicional no pinta también 20.
+//dentro de la condcional nos pinta 20. Esta vriable está viviendo dentro del if, y una vez que se sale de este scope if, muere.
+//fuera de la condicional nos pinta 10. Esto está de forma global
 //la variable dentro de la condicional varia el valor del var inicial
